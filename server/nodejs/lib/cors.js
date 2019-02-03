343c6532e6883d1e712022a658c86cd90ad97f6b
Dennis Jefferson <dennismjefferson@gmail.com>
2019-02-03 14:48:00 -0800

    Enable cors progress

--- server/nodejs/lib/cors.js
+++ server/nodejs/lib/cors.js
@@ -2,17 +2,10 @@ const cors = require('cors');
 
 var whitelist = ['http://localhost:3000', 'http://localhost:3077'];
 var corsOptions = {
-  origin: function(origin, callback) {
-    console.log('origin', origin);
-
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  credentials: true,
+  origin: whitelist
 };
 
 module.exports = function(app) {
-  app.use(cors());
+  app.use(cors(corsOptions));
 };
